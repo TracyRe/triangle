505cc7bc24543c0287b91ca1055f4ba72ca4f3c5
Tracy Reith <treith@treith.com>
2019-01-18 21:55:44 -0800

    Add js code for display of results - not yet for evaluation of input

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,30 @@
 $(document).ready(function(){
   $("form#triangle").submit(function(event){
+    var a = parseInt($("input#sideA").val());
+    var b = parseInt($("input#sideB").val());
+    var c = parseInt($("input#sideC").val());
 
-
+    if () {
+      $(".not-triangle").show();
+      $(".equilateral").hide();
+      $(".isosceles").hide());
+      $(".scalene").hide();
+    } else if () {
+      $(".not-triangle").hide();
+      $(".equilateral").show();
+      $(".isosceles").hide());
+      $(".scalene").hide();
+    } else if () {
+      $(".not-triangle").hide();
+      $(".equilateral").hide();
+      $(".isosceles").show());
+      $(".scalene").hide();
+    } else {
+      $(".not-triangle").hide();
+      $(".equilateral").hide();
+      $(".isosceles").hide());
+      $(".scalene").show();
+    }
   event.preventDefault();
   });
 });
